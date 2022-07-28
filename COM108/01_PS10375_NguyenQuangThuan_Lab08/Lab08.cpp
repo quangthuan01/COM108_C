b28ddc5cb1cb44a0652a89a269b97cb73cb3eb78
_thuan01_ <76669619+quangthuan01@users.noreply.github.com>
2022-07-28 20:06:39 +0700

    update lab08

--- COM108/01_PS10375_NguyenQuangThuan_Lab08/Lab08.cpp
+++ COM108/01_PS10375_NguyenQuangThuan_Lab08/Lab08.cpp
@@ -1,13 +1,63 @@
 #include <stdio.h>
-
 // bảng sinh viên
 struct SinhVien
 {
-    char mssv[50];
+    int mssv;
     char fullName[50];
     char nganhHoc[100];
     float diemTb;
 };
+// hàm nhập danh sách sinh viên
+void inPut(SinhVien sinhVien[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("\n\nVui long nhap thong tin sinh vien thu: %d\n", i + 1);
+        printf("\nVui long nhap MSSV: ");
+        scanf("%d", &sinhVien[i].mssv);
+        fflush(stdin);
+        printf("Vui long nhap ten sinh vien: ");
+        gets(sinhVien[i].fullName);
+        fflush(stdin);
+        printf("Vui long nhap nganh hoc: ");
+        gets(sinhVien[i].nganhHoc);
+        fflush(stdin);
+        printf("Vui long nhap diem TB: ");
+        scanf("%f", &sinhVien[i].diemTb);
+    }
+}
+// hàm xuất danh sách sinh viên đã lưu
+void outPut(SinhVien sinhVien[], int n)
+{
+    printf("\n\nSinh vien da duoc them vao la:");
+    printf("\n\tSTT \tMSSV \tTENSV \tNganhHoc \tDiemTB\n");
+    for (int i = 0; i < n; i++)
+    {
+        // xuất thông tin sinh viên
+
+        printf("\n\t%d \t%d \t%s \t%s \t%.1f\n",
+               i + 1, sinhVien[i].mssv, sinhVien[i].fullName,
+               sinhVien[i].nganhHoc, sinhVien[i].diemTb);
+    }
+}
+// hàm tìm kiếm sinh viên theo mã sinh viên
+void searchStudent(SinhVien sinhVien[], int n)
+{
+    int search;
+    printf("\n\nVui long nhap MSSV ban muon tim: ");
+    scanf("%d", &search);
+    printf("\n\tSTT \tMSSV \tTENSV \tNganhHoc \tDiemTB\n");
+    for (int i = 0; i < n; i++)
+    {
+        if (sinhVien[i].mssv == search) // nếu mã số sinh viên = mã số đã nhập thì in ra màn hình
+        {
+            // tìm kiếm sinh viên
+            printf("\n\t%d \t%d \t%s \t%s \t%.1f\n",
+                   i + 1, sinhVien[i].mssv, sinhVien[i].fullName,
+                   sinhVien[i].nganhHoc, sinhVien[i].diemTb);
+        }
+    }
+}
 // hàm đổi vị trí sinh viên
 void ChangeStudent(SinhVien &x, SinhVien &y)
 {
@@ -15,88 +65,48 @@ void ChangeStudent(SinhVien &x, SinhVien &y)
     x = y;
     y = temp;
 };
-int main()
+// hàm sắp xếp vị trí sinh viên theo điểm trung bình
+void changeStudent(SinhVien sinhVien[], int n)
 {
-    int n;
-    printf("\n\n=======================================================\n\n");
-    printf("\nVui long nhap so luong sinh vien: ");
-    scanf("%d", &n);
-    fflush(stdin);
-    SinhVien SV[n];
-    for (int i = 0; i < n; i++)
-    {
-        printf("\n\nVui long nhap thong tin sinh vien thu: %d\n", i + 1);
-        printf("\nVui long nhap MSSV: ");
-        gets(SV[i].mssv);
-        fflush(stdin);
-        printf("Vui long nhap Ho va Ten sinh vien: ");
-        gets(SV[i].fullName);
-        fflush(stdin);
-        printf("Vui long nhap nganh hoc: ");
-        gets(SV[i].nganhHoc);
-        fflush(stdin);
-        printf("Vui long nhap diem TB: ");
-        scanf("%f", &SV[i].diemTb);
-        fflush(stdin);
-    }
-    // bài 1 in ra màn hình
-    printf("\n\nLab08_Bai1");
-    for (int i = 0; i < n; i++)
-    {
-        // xuất thông tin sinh viên
-
-        printf("\n\nSTT %d\n", i + 1);
-        printf("\nMSSV: %s", SV[i].mssv);
-        printf("\nFull Name: %s", SV[i].fullName);
-        printf("\nNganh hoc: %s", SV[i].nganhHoc);
-        printf("\nDiem TB: %.1f", SV[i].diemTb);
-    }
-    // sắp xếp sinh viên
+    // sắp xếp sinh viên theo điểm trung bình
     for (int i = 0; i < n; i++)
     {
         for (int j = i + 1; j < n; j++)
         {
-            if (SV[j].diemTb > SV[i].diemTb)
-                ChangeStudent(SV[j], SV[i]);
+            if (sinhVien[j].diemTb > sinhVien[i].diemTb)
+                ChangeStudent(sinhVien[j], sinhVien[i]);
         }
     }
-    // bài 2 changeStudent
-    printf("\n\n\nLab08_Bai2");
+    printf("\n\nDanh sach sinh vien da duoc sap xep la:");
+    printf("\n\tSTT \tMSSV \tTENSV \tNganhHoc \tDiemTB\n");
     for (int i = 0; i < n; i++)
     {
         // xuất thông tin sinh viên
-
-        printf("\n\nSTT %d\n", i + 1);
-        printf("\nMSSV: %s", SV[i].mssv);
-        printf("\nFull Name: %s", SV[i].fullName);
-        printf("\nNganh hoc: %s", SV[i].nganhHoc);
-        printf("\nDiem TB: %.1f", SV[i].diemTb);
+        printf("\n\t%d \t%d \t%s \t%s \t%.1f\n",
+               i + 1, sinhVien[i].mssv, sinhVien[i].fullName,
+               sinhVien[i].nganhHoc, sinhVien[i].diemTb);
     }
-
-    // bai 3 tìm kiếm sinh viên
-    printf("\n\nLab08_Bai3 %d\n");
-    char search[50];
-    printf("\n\n Vui long nhap MSSV ban muon tim: ");
-    scanf("%s", &search);
-    for (int i = 0; i < n; i++)
+}
+// main
+int main()
+{
+    SinhVien sinhVien[10];
+    int n;
+    printf("\n\n=======================================================\n\n");
+    do
     {
-        if (search != SV[i].mssv)
-        {
-            printf("\n\nKhong co sinh vien ban muon tim trong danh sach");
-            break;
-        }
-        else
-        {
-            printf("\n\nDuoi day la sinh vien ban muon tim: ");
-            printf("\nMSSV: %s", SV[i].mssv);
-            printf("\nFull Name: %s", SV[i].fullName);
-            printf("\nNganh hoc: %s", SV[i].nganhHoc);
-            printf("\nDiem TB: %.1f", SV[i].diemTb);
-            break;
-            
-        }
-    }
+        printf("\nVui long nhap so luong sinh vien: ");
+        scanf("%d", &n);
+    } while (n < 1 || n > 50);
 
+    // nhập sinh vien
+    inPut(sinhVien, n);
+    // xuất sinh viên
+    outPut(sinhVien, n);
+    // sắp xếp sinh viên đã nhập
+    changeStudent(sinhVien, n);
+    // tìm kiếm sinh viên
+    searchStudent(sinhVien, n);
     printf("============================================");
     return 0;
 }
